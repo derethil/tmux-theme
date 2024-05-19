1224a8a6518996554deef0ba7b47ab9d7a25b3e4
Jaren Glenn <jarenglenn@gmail.com>
2024-05-19 15:16:11 -0400

    feat: convert to kanagawa

--- status/session.sh
+++ status/session.sh
@@ -2,9 +2,9 @@ show_session() {
   local index icon color text module
 
   index=$1
-  icon=$(get_tmux_option "@catppuccin_session_icon" "")
-  color=$(get_tmux_option "@catppuccin_session_color" "#{?client_prefix,$thm_red,$thm_green}")
-  text=$(get_tmux_option "@catppuccin_session_text" "#S")
+  icon=$(get_tmux_option "@kanagawa_session_icon" "")
+  color=$(get_tmux_option "@kanagawa_session_color" "#{?client_prefix,$thm_red,$thm_green}")
+  text=$(get_tmux_option "@kanagawa_session_text" "#S")
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
 
